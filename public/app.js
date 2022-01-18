123cf1234f63b0a7910a3c580c16dc8f9b15d54a
Lawe02 <lawza02@gmail.com>
2022-01-19 00:03:24 +0100

    fungerar nu med async await

--- public/app.js
+++ public/app.js
@@ -3,28 +3,35 @@
 // let req = new XMLHttpRequest();
 // req.open('GET', dataURL); 
 // req.responseType='json'; 
-// req.send(); 
-fetch('data.json')
-.then(function(resp){
-   return resp.json();
-})
-.then(function(data) {
-    
-      console.log('data recived')
-      console.log(data);
- 
-      const obj = data;
-      const lst = document.createElement('ul');
- 
-      for(let i = 0; i < data.length; i++) {
- 
-         const liTem = document.createElement('li');
-         liTem.textContent = data[i].Make;
-         lst.appendChild(liTem);
-      };
- 
-      document.getElementById('li').appendChild(lst);
- 
-})
- 
+// req.send();
 
+async function myFunction() {
+   let response = await fetch('data.json');
+
+   if (!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`);  
+   }
+
+  const data = await response.json()
+  return data;
+}
+
+myFunction()
+.then(data => {
+   
+const lst = document.createElement('ul');
+ 
+for(let i = 0; i < data.length; i++) {
+
+   const liTem = document.createElement('li');
+   liTem.textContent = data[i].Make;
+   lst.appendChild(liTem);
+};
+
+document.getElementById('li').appendChild(lst);
+})
+  
+ 
+ 
+ 
+      
